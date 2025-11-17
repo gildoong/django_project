92e05f8696441d94843dada2ad02bbff754cf33e
gildoong <leesy@gmail.com>
2025-11-17 15:11:27 +0000

    Remove manual mysql PVC

--- config/settings.py
+++ config/settings.py
@@ -78,10 +78,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'root1234',
+        'HOST': 'mysql',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
